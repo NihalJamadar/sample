8845e2506f301891fc92de5e9338bfc323bbe3a0
nihaljamadar <nihaljamadar95@gmail.com>
2018-08-23 08:45:41 +0530

    //Arithmatic

---
+++ Addition.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+ 
+class Addition
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+ 
+      System.out.println("Enter two integers to calculate their sum");
+      Scanner in = new Scanner(System.in);
+ 
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x + y;
+ 
+      System.out.println("Sum of the integers = " + z);
+   }
+}
